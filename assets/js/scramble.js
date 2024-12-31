3f0444c41a98a3d84c1c51acd9758a7140a4dfff
b0kch01 <nath49ers@gmail.com>
2024-12-30 20:17:52 -0800

    playing with animations

--- assets/js/scramble.js
+++ assets/js/scramble.js
@@ -1,37 +1,56 @@
-function scrambleTextAnimation(container, targetText, charSet, flipProbability) {
-    // Convert charSet and targetText into arrays of grapheme clusters
-    const charArray = Array.from(charSet);
-    const chars = Array.from(targetText);
+function scrambleTextAnimation(
+  container,
+  targetText,
+  charSet,
+  flipProbability,
+) {
+  // Convert charSet and targetText into arrays of grapheme clusters
+  const charArray = Array.from(charSet);
+  const chars = Array.from(targetText);
 
-    const randomChar = () => charArray[Math.floor(Math.random() * charArray.length)];
+  const randomChar = () =>
+    charArray[Math.floor(Math.random() * charArray.length)];
 
-    let scrambledText = chars.map(() => randomChar());
+  let scrambledText = chars.map(() => randomChar());
+  container.textContent = scrambledText.join("");
+
+  const interval = setInterval(() => {
+    let isDone = true;
+
+    scrambledText = scrambledText.map((currentChar, index) => {
+      if (currentChar !== chars[index]) {
+        isDone = false;
+        return Math.random() < flipProbability ? chars[index] : randomChar();
+      }
+      return currentChar;
+    });
+
     container.textContent = scrambledText.join("");
 
-    const interval = setInterval(() => {
-        let isDone = true;
-
-        scrambledText = scrambledText.map((currentChar, index) => {
-            if (currentChar !== chars[index]) {
-                isDone = false;
-                return Math.random() < flipProbability ? chars[index] : randomChar();
-            }
-            return currentChar;
-        });
-
-        container.textContent = scrambledText.join("");
-
-        if (isDone) {
-            clearInterval(interval);
-        }
-    }, 40); // Adjust interval duration for animation speed
+    if (isDone) {
+      clearInterval(interval);
+    }
+  }, 40); // Adjust interval duration for animation speed
 }
-
 
 const text = document.querySelector(".fancy-font > .text");
 const emoji = document.querySelector(".fancy-font > .emoji");
-if (text) {
-    scrambleTextAnimation(text, "Hello, I'm Matthew Leipzig!", "@#&$", 0.2);
-    scrambleTextAnimation(emoji, "👋", "🤚🖐✋🖖👌🤌🤏✌🤞🫰🤙🫵👍👎👊🤜🤛🫳🫴👏🙌👐🤲🙏🤝", 0.1)
-} else
-    console.log("Container not found");
+if (false && text) {
+  scrambleTextAnimation(
+    text,
+    "Hello! I'm Matt",
+    "abcdefghijklmnopqrstuvwxyz",
+    0.1,
+  );
+  scrambleTextAnimation(
+    emoji,
+    "👋",
+    "🤚🖐✋🖖👌🤌🤏✌🤞🫰🤙🫵👍👎👊🤜🤛🫳🫴👏🙌👐🤲🙏🤝",
+    0.1,
+  );
+} else console.log("Container not found");
+
+AOS.init({
+  easing: "ease-out-quart",
+  duration: 1000,
+});
